72c15da26bb889955b225da47c00d0afa2d22f5c
Yuuki Wesp <ls-micro@ya.ru>
2025-01-03 22:53:01 +0300

    Add meStore with ExtendedUser type and init function
    A new store 'meStore' is added with an ExtendedUser type that includes additional status options. The store contains an 'init' function to initialize user data and listen for specific events using the bus.

---
+++ src/store/meStore.ts
@@ -0,0 +1,34 @@
+import { defineStore } from "pinia";
+import { useApi } from "./apiStore";
+import { ref } from "vue";
+import { useBus } from "./busStore";
+import { logger } from "@/lib/logger";
+
+export type ExtendedUser = { currentStatus: 'Offline' | 'Online' | 'Away' | 'InGame' | 'Listen' | 'TouchGrass' | 'DoNotDisturb' } & IUser;
+
+export const useMe = defineStore("me", () => {
+  const api = useApi();
+  const bus = useBus();
+  const me = ref(null as (ExtendedUser | null));
+  async function getMe() {
+    return await api.userInteraction.GetMe();
+  }
+
+
+  async function init() {
+    me.value = { currentStatus: "Offline", ...(await getMe()) };
+
+    logger.info("Received user info ", me.value);
+    bus.onUserEvent<WelcomeCommander>("WelcomeCommander", (e) => {
+        logger.box(`Welcome commander, ${e.welcomeMessage}`);
+        me.value!.currentStatus = e.status;
+    });
+
+    bus.doListenMyEvents();
+  }
+
+  return {
+    me,
+    init
+  };
+});
