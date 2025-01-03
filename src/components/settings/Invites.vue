b1b3fea76bb9ed605d5f6c8435c8db8b8cae39d3
Yuuki Wesp <ls-micro@ya.ru>
2025-01-03 22:52:09 +0300

    Add Invite functionality to manage invite codes
    - Added a new component for managing invite codes
    - Implemented logic to add and remove invite codes
    - Included styling for the invite code management interface

---
+++ src/components/settings/Invites.vue
@@ -0,0 +1,98 @@
+<template>
+    <div class="flex flex-col space-y-4">
+        <div class="flex justify-between items-center">
+            <h2 class="text-lg font-semibold">Invite Codes</h2>
+            <Button @click="addInvite" :disabled="loading" variant="default" class="px-4 py-2">
+                Add Invite
+            </Button>
+        </div>
+
+        <ul class="space-y-2">
+            <div class="spinner-container" v-if="loading">
+                <AtomSpinner class="text-center" />
+            </div>
+            <li v-if="!loading" v-for="invite in invites" :key="invite.code.inviteCode"
+                class="flex justify-between items-center rounded-lg px-4 py-2">
+                <Input readonly :model-value="`https://argon.gl/i/${invite.code}/`" />
+                <Button @click="removeInvite(invite.code.inviteCode)" variant="ghost" class="text-red-500">
+                    Remove
+                </Button>
+            </li>
+            <li v-if="!loading && invites.length == 0">
+                <br />
+                <br />
+                <p class="text-sm text-muted-foreground text-center">
+                    No any created invite codes
+                </p>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { Button } from '@/components/ui/button';
+import { useServerStore } from '@/store/serverStore';
+import { AtomSpinner } from 'epic-spinners'
+import { Input } from '@/components/ui/input';
+const servers = useServerStore();
+const loading = ref(true);
+
+onMounted(async () => {
+    await refreshInvites();
+});
+
+
+const refreshInvites = async () => {
+    loading.value = true;
+    const result = await servers.getServerInvites();
+    invites.value = result;
+
+    console.log(invites);
+    loading.value = false;
+}
+
+const invites = ref<InviteCodeEntity[]>([]);
+
+const addInvite = async () => {
+    await servers.addInvite();
+    await refreshInvites();
+};
+
+const removeInvite = (id: string) => {
+};
+</script>
+
+<style scoped>
+.spinner-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.flex {
+    display: flex;
+}
+
+.space-y-4>*+* {
+    margin-top: 1rem;
+}
+
+.bg-gray-800 {
+    background-color: #1f2937;
+}
+
+.rounded-lg {
+    border-radius: 0.5rem;
+}
+
+.px-4 {
+    padding-left: 1rem;
+    padding-right: 1rem;
+}
+
+.py-2 {
+    padding-top: 0.5rem;
+    padding-bottom: 0.5rem;
+}
+</style>
