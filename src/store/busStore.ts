e9d1ee45e82d50a8b5892798739bc3499d198a33
Yuuki Wesp <ls-micro@ya.ru>
2025-01-03 22:52:51 +0300

    Add user event handling and logging for received events
    - Added a new userEventBus to handle user events
    - Implemented doListenMyEvents function to listen for user events
    - Created onUserEvent function to subscribe to user events
    - Included logging of received events with logger

--- src/store/busStore.ts
+++ src/store/busStore.ts
@@ -2,11 +2,13 @@ import { defineStore } from "pinia";
 import { Subject, Subscription } from "rxjs";
 import { filter } from "rxjs/operators";
 import { useApi } from "./apiStore";
+import { logger } from "@/lib/logger";
 
 export type EventWithServerId<T> = {serverId: string} & T;
 
 export const useBus = defineStore("bus", () => {
   const argonEventBus = new Subject<{serverId: string} & IArgonEvent>();
+  const userEventBus = new Subject<IArgonEvent>();
 
   const controller = new AbortController();
   const { signal } = controller;
@@ -16,15 +18,36 @@ export const useBus = defineStore("bus", () => {
   async function doListenServer(id: string) {
     const handle = await api.eventBus.SubscribeToServerEvents(id);
     for await(const e of handle) {
+      logger.log(`Received event, ${e.EventKey}`, e);
       argonEventBus.next({ serverId: id, ...e });
       if (signal.aborted)
         break;
     }
   }
 
-  function on<T extends IArgonEvent>(key: T["EventKey"], callback: (event: EventWithServerId<T> ) => void): Subscription {
+  async function doListenMyEvents() {
+    const handle = await api.eventBus.SubscribeToMeEvents();
+    for await(const e of handle) {
+      logger.log(`Received event, ${e.EventKey}`, e);
+      userEventBus.next(e);
+      if (signal.aborted)
+        break;
+    }
+  }
+
+  function listenEvents(id: string) {
+    doListenServer(id);
+  }
+
+  function onServerEvent<T extends IArgonEvent>(key: T["EventKey"], callback: (event: EventWithServerId<T> ) => void): Subscription {
     return argonEventBus
       .pipe(filter((event): event is EventWithServerId<T> => event.EventKey === key))
+      .subscribe(callback);
+  }
+
+  function onUserEvent<T extends IArgonEvent>(key: T["EventKey"], callback: (event: T ) => void): Subscription {
+    return userEventBus
+      .pipe(filter((event): event is T => event.EventKey === key))
       .subscribe(callback);
   }
 
@@ -32,5 +55,5 @@ export const useBus = defineStore("bus", () => {
     controller.abort(reason);
   }
  
-  return { argonEventBus, doListenServer, closeAllSubscribes, on };
+  return { argonEventBus, listenEvents, closeAllSubscribes, onServerEvent, onUserEvent, doListenMyEvents };
 });
