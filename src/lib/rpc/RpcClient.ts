9d4bcba4bff22b11c17aa07bbf6c2dab66e1eed8
Yuuki Wesp <ls-micro@ya.ru>
2025-01-07 11:16:33 +0300

    Add timeout to transport, improve error handling & logging
    - Added a timeout of 600 seconds to the transport initialization
    - Improved error handling and logging for stream responses & connection issues
    - Implemented bi-directional subscription method with interval sending

--- src/lib/rpc/RpcClient.ts
+++ src/lib/rpc/RpcClient.ts
@@ -7,7 +7,7 @@ export class RpcClient {
   private client: ArgonTransportClient;
 
   constructor(baseUrl: string) {
-    const transport = new GrpcWebFetchTransport({ baseUrl, format: "text" });
+    const transport = new GrpcWebFetchTransport({ baseUrl, format: "text", timeout: 60000 * 10 });
     this.client = new ArgonTransportClient(transport);
   }
 
@@ -36,8 +36,6 @@ export class RpcClient {
                 `${response.status.code} - ${response.status.detail || "Unknown error occurred."}`
               );
             }
-
-            logger.log(response);
 
             if (response.response.payload.length === 0)
               return null;
@@ -68,13 +66,25 @@ export class RpcClient {
               },
               { meta: { authorize: authStore.token ?? "" } }
             );
+            (window as any)["subscribe_bi"] = this.subscribe_bi;
+            this.subscribe_bi(serviceName, String(methodName), args);
 
             return {
               [Symbol.asyncIterator]: async function* () {
-                for await (const response of stream.responses) {
-                  if (response.payload.length === 0)
-                    continue;
-                  yield decode(response.payload) as IArgonEvent;
+                const start = performance.now();
+                try {
+                  
+                  for await (const response of stream.responses) {
+                    if (response.payload.length === 0)
+                      continue;
+                    yield decode(response.payload) as IArgonEvent;
+                  }
+                } catch (error) {
+                  logger.error("Stream connection lost or an error occurred:", error);
+            
+                } finally {
+                  const end = performance.now();
+                  logger.log(`Stream has been closed or completed, startTime: ${start}, endTime: ${end}, elapsed: ${(end - start) / 1000} seconds`);
                 }
               },
             };
@@ -82,6 +92,32 @@ export class RpcClient {
         },
       }
     ) as T;
+  }
+
+  async subscribe_bi(
+    serviceName: string,
+    methodName: string,
+    args: any[]
+  ) {
+    const authStore = useAuthStore();
+    const payload = encode(args);
+
+    const stream = this.client.biDirectSubscribe(
+      { meta: { authorize: authStore.token ?? "" } }
+    );
+
+    setInterval(() => {
+      stream.requests.send({
+        interface: serviceName,
+        method: methodName,
+        payload: payload,
+      });
+      console.log("send success");
+    }, 100);
+
+    for await (const r of stream.responses) {
+      console.log("bi direct called return", r);
+    }
   }
 
   async *subscribe(
