a1afa19d1c5b167a107377fe67d11bdab7074aeb
Yuuki Wesp <ls-micro@ya.ru>
2025-02-04 02:11:22 +0300

    Add Sentry error tracking initialization and remove redundant code.
    - Added Sentry error tracking initialization using the Sentry Vue package.
    - Removed redundant code related to restarting the app and initializing Sentry.

--- src/main.ts
+++ src/main.ts
@@ -6,18 +6,12 @@ import router from "./router";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 import { MotionPlugin } from "@vueuse/motion";
 import { registerDirectives } from "./lib/pexDirective";
+import * as Sentry from "@sentry/vue";
 
 let pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
 var app = createApp(App);
-
-const restartApp = () => {
-  app.unmount();
-  app = createApp(App);
-  let pinia = createPinia();
-  pinia.use(piniaPluginPersistedstate);
-
-  /*Sentry.init({
+Sentry.init({
   app,
   dsn: "https://27e1482fc59fff31b5a7263805f7ecac@sentry.svck.dev/7",
   integrations: [
@@ -31,31 +25,6 @@ const restartApp = () => {
   replaysSessionSampleRate: 1.0,
   replaysOnErrorSampleRate: 1.0,
 });
-*/
-  app.use(router);
-  app.use(pinia);
-  app.use(MotionPlugin);
-  registerDirectives(app);
-  app.mount("#app");
-}
-
-(window as any)["restartApp"] = restartApp;
-
-/*Sentry.init({
-  app,
-  dsn: "https://27e1482fc59fff31b5a7263805f7ecac@sentry.svck.dev/7",
-  integrations: [
-    Sentry.browserTracingIntegration({ router }),
-    Sentry.replayIntegration({
-      maskAllText: false,
-      blockAllMedia: false,
-    }),
-  ],
-  tracesSampleRate: 1.0,
-  replaysSessionSampleRate: 1.0,
-  replaysOnErrorSampleRate: 1.0,
-});
-*/
 app.use(router);
 app.use(pinia);
 app.use(MotionPlugin);
