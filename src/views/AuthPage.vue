e315e5b0386e1ff73ad4b82f40f3ed0141c72431
Yuuki Wesp <ls-micro@ya.ru>
2025-02-07 19:01:50 +0300

    Add PixelCard component and update quotes in AuthPage.vue
    - Imported PixelCard component
    - Updated quotes with new text and authors

--- src/views/AuthPage.vue
+++ src/views/AuthPage.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import PixelCard from '@/components/PixelCard.vue';
 import UserAuthForm from '@/components/UserAuthForm.vue'
 import router from '@/router';
 import { useAuthStore } from '@/store/authStore';
@@ -6,9 +7,12 @@ import { onMounted } from 'vue';
 
 const quotes = [
     { text: "Если намочить руку — то она будет мокрая.", author: "Арам" },
-    { text: "Попробуй перед сном читать в зеркало заклинание \"завалиебало завалиебало завалиебало\"", author: "Татарин" },
-    { text: "Бля я стану самым сильным питон разработчиком. Но пока только читаю резеро.", author: "Баров" },
-    { text: "У тебя же нет мамы, прости", author: "Ефим" }
+    { text: "А клыки нам даны, чтобы ... кору деревьев отгрызать?", author: "Miniature" },
+    { text: "СЫН БЛЯДИ КОНЧЕННЫЙ УЕБОК Я ПО НОРМАЛЬНОМУ СПРОСИЛ БЛЯТЬ", author: "Беляш" },
+    { text: "Блядь, ёбанный Юки", author: "Мурзилка" },
+    { text: "Ненавижу блять солнце", author: "Юки" }
+
+
 ];
 const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
 const authStore = useAuthStore();
@@ -17,37 +21,12 @@ onMounted(() => {
         router.push({ path: "/master.pg" });
         return;
     }
-
-    const el = document.querySelector("#background") as any;
-    let targetX = 0;
-    let targetY = 0;
-    let currentX = 0;
-    let currentY = 0;
-    const lerpSpeed = 0.1;
-    function animate() {
-        currentX += (targetX - currentX) * lerpSpeed;
-        currentY += (targetY - currentY) * lerpSpeed;
-
-        el.style.backgroundPosition = `${50 + currentX}% ${50 + currentY}%`;
-        requestAnimationFrame(animate);
-    }
-
-    document.addEventListener('mousemove', function (e) {
-        const mouseX = e.clientX;
-        const mouseY = e.clientY;
-        const windowWidth = window.innerWidth;
-        const windowHeight = window.innerHeight;
-        targetX = (mouseX / windowWidth - 0.5) * 20;
-        targetY = (mouseY / windowHeight - 0.5) * 20;
-    });
-
-    animate();
 })
 </script>
 
 <template>
-    <div v-motion-slide-visible-once-top :duration="200"
-        class="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+    <div v-motion-slide-visible-once-top :duration="200" style="overflow: hidden;"
+        class="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0" >
         <!-- <a href="/examples/authentication" :class="cn(
             buttonVariants({ variant: 'ghost' }),
             'absolute right-4 top-4 md:right-8 md:top-8',
@@ -55,12 +34,8 @@ onMounted(() => {
             Login
         </a>-->
         <div class="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-            <div class="absolute inset-0 bg-zinc-900" id="background" style="
-            background-image: url(https://i.pinimg.com/originals/e1/ab/c9/e1abc95c09b5f59ae760038b3be7598c.jpg);
-            background-repeat: no-repeat;
-            background-size: cover;
-            background-position: center;
-            transition: background-position 0.1s ease-out;" />
+
+            <PixelCard class="absolute inset-0 bg-zinc-900 " id="background" style="position: absolute;"/>
             <div class="relative z-20 flex items-center text-lg font-medium">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor"
                     strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="mr-2 h-6 w-6">
