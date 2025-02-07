d262eefd99c5e43f8ddabd9a1ce4985c41e3cbee
Yuuki Wesp <ls-micro@ya.ru>
2025-02-07 19:02:07 +0300

    Refactor event bus handling and WebSocket stream creation
    - Added methods for WebSocket stream creation
    - Improved error handling and retry mechanism in event bus processing

--- src/lib/rpc/RpcClient.ts
+++ src/lib/rpc/RpcClient.ts
@@ -3,6 +3,61 @@ import { encode, decode } from "@msgpack/msgpack";
 import { ArgonTransportClient } from "../proto/transport.client";
 import { useAuthStore } from "@/store/authStore";
 import { logger } from "../logger";
+import { useSystemStore } from "@/store/systemStore";
+import { fromEvent, tap } from "rxjs";
+import delay from "../delay";
+
+export function buildAtUrl(
+  upgrade: string, aat: string, 
+  sequence?: number, eventId?: number, 
+  srv?: string) 
+{
+  const sys = useSystemStore();
+  const args = [];
+
+  if (srv) 
+    args.push(`&srv=${srv}`);
+  if (sequence) 
+    args.push(`&sequence=${sequence}`);
+  if (eventId) 
+    args.push(`&eventId=${eventId}`);
+
+  return new URL(`${sys.preferUseWs ? `/$at.ws` : `/$at.wt`}?aat=${aat}${args.join()}`,
+   `https://${upgrade}`);
+}
+
+export class WsStream {
+  private socket: WebSocket;
+  private reader: ReadableStreamDefaultReader<Uint8Array>;
+
+  constructor(url: URL) {
+    this.socket = new WebSocket(url);
+
+    const stream = new ReadableStream<Uint8Array>({
+      start: (controller) => {
+        this.socket.onmessage = (event) => controller.enqueue(event.data);
+        this.socket.onclose = () => controller.close();
+        this.socket.onerror = (e) => controller.error(e);
+      }
+    });
+
+    this.reader = stream.getReader();
+  }
+
+  send(data: ArrayBufferLike) {
+    this.socket.send(data);
+  }
+
+  async read(): Promise<ReadableStreamReadResult<Uint8Array>>{
+    return await this.reader.read();
+  }
+
+  close() {
+    this.socket.close();
+  }
+}
+
+
 export class RpcClient {
   private client: ArgonTransportClient;
   private baseUrl: string;
@@ -19,148 +74,160 @@ export class RpcClient {
 
   create<T>(serviceName: string): T {
     return new Proxy(
-      {},
-      {
-        get: (_, methodName) => {
-          return async (...args: any[]) => {
-            const authStore = useAuthStore();
-            const payload = encode(args, {
-              useBigInt64: true,
-            });
+        {},
+        {
+            get: (_, methodName) => {
+                return async (...args: any[]) => {
+                    const authStore = useAuthStore();
+                    const sys = useSystemStore();
+                    const payload = encode(args, {
+                        useBigInt64: true,
+                    });
 
-            const response = await this.client.unary(
-              {
-                interface: serviceName,
-                method: String(methodName),
-                payload: payload,
-              },
-              { meta: { authorize: authStore.token ?? "" } }
-            );
 
-            if (response.status.code != "OK") {
-              throw new Error(
-                `${response.status.code} - ${response.status.detail || "Unknown error occurred."}`
-              );
-            }
+                    while(sys.hasRequestRetry("argon-transport", methodName.toString())) {
+                      logger.warn("Awaiting unlock request retry trigger...");
+                      await delay(1000);
+                    }
 
-            if (response.response.payload.length === 0) return null;
+                    const maxRetries = 100; 
+                    const baseDelay = 500;
 
-            return decode(response.response.payload);
-          };
-        },
-      }
+                    for (let attempt = 0; attempt <= maxRetries; attempt++) {
+                        if (attempt > 1) {
+                          sys.startRequestRetry("argon-transport", methodName.toString());
+                        }
+
+                        try {
+                            const response = await this.client.unary(
+                                {
+                                    interface: serviceName,
+                                    method: String(methodName),
+                                    payload: payload,
+                                },
+                                { meta: { authorize: authStore.token ?? "" } }
+                            );
+
+                            if (response.status.code !== "OK") {
+                                throw new Error(
+                                    `${response.status.code} - ${response.status.detail || "Unknown error occurred."}`
+                                );
+                            }
+
+                            if (response.response.payload.length === 0) return null;
+
+                            sys.stopRequestRetry("argon-transport", methodName.toString());
+                            return decode(response.response.payload);
+                        } catch (error) {
+                            if (attempt === maxRetries) throw error;
+                            await new Promise((res) => setTimeout(res, baseDelay * Math.pow(2, attempt)));
+                        }
+                    }
+                };
+            },
+        }
     ) as T;
+}
+  
+  eventBus<T>(): T {
+    const sys = useSystemStore();
+    if (sys.preferUseWs)
+      return this.eventBusWs<T>();
+    return this.eventBusWt<T>();
   }
 
-  eventBus<T>(): T {
+  eventBusWs<T>(): T {
     return new Proxy(
       {},
       {
         get: (_, methodName) => {
-          return async (
-            ...args: any[]
-          ): Promise<AsyncIterable<IArgonEvent>> => {
+          return async (...args: any[]): Promise<AsyncIterable<IArgonEvent>> => {
             const authStore = useAuthStore();
-            const wtUrl = new URL(`${this.baseUrl}/$wt`, this.baseUrl);
+            const wtUrl = new URL(`${this.baseUrl}/$at.http`, this.baseUrl);
+            const baseAddr = this.baseUrl;
+            const sys = useSystemStore();
 
-            const attResponse = await fetch(wtUrl, {
-              method: "GET",
-              headers: {
-                Authorization: authStore.token!,
-              },
-            });
-
-            if (!attResponse.ok) {
-              logger.error(
-                `Error: ${attResponse.status} - ${attResponse.statusText}`
-              );
-              throw new Error();
-            }
-
-            logger.log(attResponse);
-
-            let upgrade = attResponse.headers.get("X-Wt-Upgrade");
-            const fingerprint = attResponse.headers.get("X-Wt-Fingerprint");
-            const aat = attResponse.headers.get("X-Wt-AAT");
-
-            logger.warn(
-              `Upgrade: ${upgrade} with ${fingerprint} fingerprint, using ${aat} AAT`
-            );
-
-            if (upgrade === "localhost") {
-              upgrade = this.baseUrl.replace("https://", "");
-            }
-
-            const transportUrl =
-              methodName === "SubscribeToMeEvents"
-                ? new URL(`transport.wt?aat=${aat}`, `https://${upgrade}`)
-                : new URL(
-                    `transport.wt?aat=${aat}&srv=${args[0]}`,
-                    `https://${upgrade}`
-                  );
-
-            const certs: WebTransportHash[] = [];
-
-            if (fingerprint) {
-              certs.push({
-                value: Uint8Array.from(atob(fingerprint), (c) =>
-                  c.charCodeAt(0)
-                ),
-                algorithm: "sha-256",
+            let sequence: number | undefined = undefined;
+            let eventId: number | undefined = undefined;
+  
+            async function fetchTransportDetails() {
+              const attResponse = await fetch(wtUrl, {
+                method: "GET",
+                headers: { Authorization: authStore.token! },
               });
+  
+              if (!attResponse.ok) {
+                logger.error(`Error: ${attResponse.status} - ${attResponse.statusText}`);
+                throw new Error("Failed to fetch transport details");
+              }
+  
+              let upgrade = attResponse.headers.get("X-Wt-Upgrade")!;
+              const aat = attResponse.headers.get("X-Wt-AAT")!;
+
+              if (upgrade === "localhost") {
+                upgrade = baseAddr.replace("https://", "");
+              }
+  
+              const transportUrl = buildAtUrl(
+                upgrade, aat, sequence, eventId,
+                methodName === "SubscribeToMeEvents" ? null : args[0]);
+  
+              return { transportUrl };
             }
 
-            function createWt() {
-              return new WebTransport(transportUrl.toString(), {
-                congestionControl: "throughput",
-                allowPooling: true,
-                serverCertificateHashes: certs,
-              });
-            }
-
-            let transport = createWt();
-
-            await transport.ready;
-            logger.log("WebTransport connection established.");
-
-            const beforeUnloadHandler = (event: BeforeUnloadEvent) => {
-              transport.close({ reason: "end", closeCode: 200 });
-              //event.preventDefault();
-            };
-            window.addEventListener("beforeunload", beforeUnloadHandler);
             return {
               [Symbol.asyncIterator]: async function* () {
-                const start = performance.now();
-                let stream = await transport.createBidirectionalStream();
-                let reader = stream.readable.getReader();
+                let reconnectDelay = 1000;
+                
                 while (true) {
+                  let transport: WebSocketStream = null!;
+                  const sub = fromEvent(window, 'beforeunload').pipe(
+                    tap(() => transport?.close())
+                  ).subscribe();
                   try {
-                    const { value, done } = await reader.read();
-                    if (done) break;
-                    if (value && value.length > 0) {
-                      try {
-                        yield decode(value) as IArgonEvent;
-                      } catch (e) {
-                        logger.warn(
-                          "Failed processing message, but stream is continue"
-                        );
+                    const { transportUrl } = await fetchTransportDetails();
+
+                    transport = new WebSocketStream(transportUrl.toString(), {
+
+                    });
+
+                    const stream = await transport.opened;
+                    const reader = stream.readable.getReader();
+
+                    sys.stopRequestRetry("argon-transport-streams", "ws");
+                    reconnectDelay = 1000;
+  
+                    while (true) {
+                      const { value, done } = await reader.read();
+
+                      if(typeof value === "string")
+                        throw new Error();
+
+                      if (done) {
+                        logger.warn("WebSocket stream ended.");
+                        break;
+                      }
+                      if (value) {
+                        try {
+                          const buffer = new Uint8Array(value);
+
+                          if (!buffer.length)
+                            continue;
+                          yield decode(new Uint8Array(value)) as IArgonEvent;
+                        } catch (e) {
+                          logger.warn("Failed to process message, but continuing stream", e);
+                        }
                       }
                     }
-                    continue;
                   } catch (error) {
-                    logger.error("WebTransport stream error:", error);
-                  } 
-
-                  const end = performance.now();
-                  logger.log(
-                    `WebTransport stream closed, elapsed: ${(end - start) / 1000} seconds`
-                  );
-                  reader.releaseLock();
-                  transport.close();
-                  transport = createWt();
-                  await transport.ready;
-                  stream = await transport.createBidirectionalStream();
-                  reader = stream.readable.getReader();
+                    logger.error("WebSocket error:", error);
+                  } finally {
+                    sub.unsubscribe();
+                  }
+                  sys.startRequestRetry("argon-transport-streams", "ws");
+                  logger.warn(`Reconnecting in ${reconnectDelay / 1000} seconds...`);
+                  await new Promise((resolve) => setTimeout(resolve, reconnectDelay));
+                  reconnectDelay = Math.min(reconnectDelay * 2, 10000);
                 }
               },
             };
@@ -169,26 +236,117 @@ export class RpcClient {
       }
     ) as T;
   }
-
-  async *subscribe(
-    serviceName: string,
-    methodName: string,
-    args: any[]
-  ): AsyncIterable<IArgonEvent> {
-    const authStore = useAuthStore();
-    const payload = encode(args);
-
-    const stream = this.client.broadcastSubscribe(
+  eventBusWt<T>(): T {
+    return new Proxy(
+      {},
       {
-        interface: serviceName,
-        method: methodName,
-        payload: payload,
-      },
-      { meta: { authorize: authStore.token ?? "" } }
-    );
+        get: (_, methodName) => {
+          return async (...args: any[]): Promise<AsyncIterable<IArgonEvent>> => {
+            const authStore = useAuthStore();
+            const wtUrl = new URL(`${this.baseUrl}/$at.http`, this.baseUrl);
+            const baseAddr = this.baseUrl;
+            const sys = useSystemStore();
 
-    for await (const r of stream.responses) {
-      yield decode(r.payload) as IArgonEvent;
-    }
+            let sequence: number | undefined = undefined;
+            let eventId: number | undefined = undefined;
+  
+            async function fetchTransportDetails() {
+              const attResponse = await fetch(wtUrl, {
+                method: "GET",
+                headers: { Authorization: authStore.token! },
+              });
+  
+              if (!attResponse.ok) {
+                logger.error(`Error: ${attResponse.status} - ${attResponse.statusText}`);
+                throw new Error("Failed to fetch transport details");
+              }
+  
+              let upgrade = attResponse.headers.get("X-Wt-Upgrade")!;
+              const fingerprint = attResponse.headers.get("X-Wt-Fingerprint");
+              const aat = attResponse.headers.get("X-Wt-AAT")!;
+  
+              if (upgrade === "localhost") {
+                upgrade = baseAddr.replace("https://", "");
+              }
+  
+              const transportUrl = buildAtUrl(
+                upgrade, aat, sequence, eventId,
+                methodName === "SubscribeToMeEvents" ? null : args[0]);
+  
+              const certs: WebTransportHash[] = [];
+              if (fingerprint) {
+                certs.push({
+                  value: Uint8Array.from(atob(fingerprint), (c) => c.charCodeAt(0)),
+                  algorithm: "sha-256",
+                });
+              }
+  
+              return { transportUrl, certs };
+            }
+  
+            async function createTransport(transportUrl: URL, certs: WebTransportHash[]) {
+              const transport = new WebTransport(transportUrl.toString(), {
+                congestionControl: "throughput",
+                allowPooling: true,
+                serverCertificateHashes: certs,
+              });
+  
+              await transport.ready;
+              logger.log("WebTransport connection established.");
+              return transport;
+            }
+  
+  
+            return {
+              [Symbol.asyncIterator]: async function* () {
+                let reconnectDelay = 1000;
+                
+                while (true) {
+                  let transport: WebTransport = null!;
+                  const sub = fromEvent(window, 'beforeunload').pipe(
+                    tap(() => transport?.close())
+                  ).subscribe();
+                  try {
+
+                    const { transportUrl, certs } = await fetchTransportDetails();
+                    let transport = await createTransport(transportUrl, certs);
+                    let stream = await transport.createBidirectionalStream();
+                    let reader = stream.readable.getReader() as ReadableStreamDefaultReader<Uint8Array>;
+  
+                    reconnectDelay = 1000;
+                    sys.stopRequestRetry("argon-transport-streams", "wt");
+  
+                    while (true) {
+                      const { value, done } = await reader.read();
+                      logger.warn(value, done);
+
+                      if (done) {
+                        logger.warn("WebTransport stream ended.");
+                        break;
+                      }
+                      if (value?.length) {
+                        try {
+                          yield decode(value) as IArgonEvent;
+                        } catch (e) {
+                          logger.warn("Failed to process message, but continuing stream", e);
+                        }
+                      }
+                    }
+                  } catch (error) {
+                    logger.error("WebTransport error:", error);
+                  } finally {
+                    sub.unsubscribe();
+                  }
+                  sys.startRequestRetry("argon-transport-streams", "wt");
+                  logger.warn(`Reconnecting in ${reconnectDelay / 1000} seconds...`);
+                  await new Promise((resolve) => setTimeout(resolve, reconnectDelay));
+                  reconnectDelay = Math.min(reconnectDelay * 2, 10000);
+                }
+              },
+            };
+          };
+        },
+      }
+    ) as T;
   }
 }
