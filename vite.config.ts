6c457deafeeb78c91844ae77327e46e0c8751009
Yuuki Wesp <ls-micro@ya.ru>
2025-02-17 01:58:17 +0300

    Update server configuration to include HTTPS settings.
    - Added HTTPS configuration with key and cert file paths.
    - Removed the cert plugin from plugins list.

--- vite.config.ts
+++ vite.config.ts
@@ -4,20 +4,21 @@ import path from 'path';
 import tailwind from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 import Icons from 'unplugin-icons/vite'
-import cert from '@vitejs/plugin-basic-ssl'
 import vueDevTools from 'vite-plugin-vue-devtools'
 export default defineConfig({
   server: {
-    port: 5005
+    port: 5005,
+    https: {
+      key: "localhost-key.pem",
+      cert: "localhost.pem"
+    }
   },
-
   css: {
     postcss: {
       plugins: [tailwind(), autoprefixer()],
     },
   },
   plugins: [
-    cert(),
     vue(),
     Icons({ compiler: 'vue3', autoInstall: true }) as any,
     vueDevTools(),
