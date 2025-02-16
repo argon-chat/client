f3ddae7be3478342ee3aa175a371d6cc8b8a28dd
Yuuki Wesp <ls-micro@ya.ru>
2025-02-17 01:58:58 +0300

    Refactor chat list UI components for better organization.
    - Restructured layout and styling of chat list components
    - Added dropdown menu for server settings and channel options
    - Improved handling of channel actions and user interactions

--- src/components/ChatList.vue
+++ src/components/ChatList.vue
@@ -1,180 +1,156 @@
 <template>
-  <div class="chat-list min-w-[250px]">
-    <div class="flex flex-col">
-      <div class="p-4 border-b border-gray-700 flex justify-between items-center" v-if="pool.selectedServer">
-        <h2 class="text-lg font-bold">{{ pool.getSelectedServer?.Name }}</h2>
-        <button class="text-gray-400 hover:text-white" @click="windows.serverSettingsOpen = true">
-          <CloverIcon class="w-5 h-5" />
-        </button>
+  <div style="display: contents;">
+    <div class="header-list rounded-t-lg overflow-hidden bg-cover bg-no-repeat bg-center contrast-125"
+      :style="{ backgroundImage: 'url(https://i.ppy.sh/f3bd9efc54c8464c7cfba57e7a8b8b6953444175/68747470733a2f2f612d7374617469632e62657374686477616c6c70617065722e636f6d2f6461726c696e672d64616e732d6c652d6672616e78782d7a65726f2d74776f2d666f6e642d642d656372616e2d32353630783936302d33333638395f38372e6a7067)' }"
+      v-if="pool.selectedServer">
 
-        <Dialog v-model:open="addChannelOpened">
-          <DialogTrigger as-child>
-            <button v-pex="'ManageBehaviour'" class="text-gray-400 hover:text-white">
-              <PlusIcon class="w-5 h-5" />
+      <div class="relative p-4 flex justify-between items-center border-b " v-if="pool.selectedServer">
+        <h2 class="text-lg font-bold relative z-10 text-white ">
+          {{ pool.getSelectedServer?.Name }}
+        </h2>
+        <DropdownMenu :modal="serverDropdownIsActive">
+          <DropdownMenuTrigger as-child class="backdrop-blur-md">
+            <button v-show="isAdmin">
+              <MoreVerticalIcon />
             </button>
-          </DialogTrigger>
-          <DialogContent class="sm:max-w-[425px]">
-            <DialogHeader>
-              <DialogTitle>Add channel</DialogTitle>
-            </DialogHeader>
-            <div class="grid gap-4 py-4">
-              <div class="grid grid-cols-4 items-center gap-4">
-                <Label for="channel-name" class="text-right">
-                  Name
-                </Label>
-                <Input id="channel-name" v-model="channelName" class="col-span-3" />
-              </div>
-              <div class="grid grid-cols-4 items-center gap-4">
-                <Label for="channel-type" class="text-right">
-                  Type
-                </Label>
-                <RadioGroup id="channel-type" v-model="channelType" :orientation="'vertical'">
-                  <div class="flex items-center space-x-2">
-                    <RadioGroupItem id="r1" value="Text" />
-                    <Label for="r1">
-                      <Badge>Text</Badge>
-                    </Label>
-                  </div>
-                  <div class="flex items-center space-x-2">
-                    <RadioGroupItem id="r2" value="Voice" />
-                    <Label for="r2">
-                      <Badge>Voice</Badge>
-                    </Label>
-                  </div>
-                  <div class="flex items-center space-x-2">
-                    <RadioGroupItem id="r3" value="Announcement" />
-                    <Label for="r3">
-                      <Badge>Announcement</Badge>
-                    </Label>
-                  </div>
-                </RadioGroup>
-              </div>
-            </div>
-            <DialogFooter>
-              <DialogClose as-child>
-                <Button type="button" variant="secondary">
-                  Cancel
-                </Button>
-              </DialogClose>
-              <Button type="submit" @click="addChannel">
-                Create Channel
-              </Button>
-            </DialogFooter>
-          </DialogContent>
-        </Dialog>
 
-
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem @click="openServerSettings">Server Settings</DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem @click="addChannelOpened = true">Add Channel</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
-
-      <div class="flex-1 overflow-y-auto py-4 ">
-
-
-
-        <div v-if="pool.selectedServer" v-for="channel in pool.activeServerChannels.value" :key="channel.Id"
-          class="px-4 py-2 hover:bg-gray-700 cursor-pointer flex flex-col " v-on:click="channelSelect(channel.Id)">
-
-          <ContextMenu>
-            <ContextMenuTrigger>
-
-
-              <div class="flex items-center justify-between group">
-                <div class="flex items-center space-x-2">
-                  <HashIcon v-if="channel.ChannelType === 'Text'" class="w-5 h-5 text-gray-400" />
-                  <Volume2Icon v-else-if="channel.ChannelType === 'Voice'" class="w-5 h-5 text-gray-400" />
-                  <AntennaIcon v-else-if="channel.ChannelType === 'Announcement'" class="w-5 h-5 text-gray-400" />
-                  <span>{{ channel?.Name }}</span>
-                </div>
-              </div>
-              <ul
-                v-if="channel.ChannelType === 'Voice' && pool.realtimeChannelUsers.has(channel.Id) && pool.realtimeChannelUsers.get(channel.Id)?.Users.size != 0"
-                class="ml-3 mt-2 space-y-2">
-                <li v-for="user in pool.realtimeChannelUsers.get(channel.Id)!.Users.values()" :key="user.UserId"
-                  class="flex items-center mt-1 text-gray-400 hover:text-white">
-                  <ArgonAvatar :fallback="user.User.DisplayName" :fileId="user.User.AvatarFileId!" :userId="user.UserId"
-                    :style="(user.isSpeaking ? 'outline: solid #45d110 2px; outline-offset: 4px; border-radius: 500px;' : '')"
-                    class="w-7 h-7 rounded-full mr-3" />
-                  <span>{{ user.User.DisplayName }}</span>
-                </li>
-              </ul>
-
-            </ContextMenuTrigger>
-            <ContextMenuContent class="w-64">
-              <ContextMenuItem inset>
-                Back
-                <ContextMenuShortcut>⌘[</ContextMenuShortcut>
-              </ContextMenuItem>
-              <ContextMenuItem inset disabled>
-                Forward
-                <ContextMenuShortcut>⌘]</ContextMenuShortcut>
-              </ContextMenuItem>
-              <ContextMenuItem inset>
-                Reload
-                <ContextMenuShortcut>⌘R</ContextMenuShortcut>
-              </ContextMenuItem>
-              <ContextMenuSub>
-                <ContextMenuSubTrigger inset>
-                  More Tools
-                </ContextMenuSubTrigger>
-                <ContextMenuSubContent class="w-48">
-                  <ContextMenuItem>
-                    Save Page As...
-                    <ContextMenuShortcut>⇧⌘S</ContextMenuShortcut>
+    </div>
+    <div class="chat-list rounded-b-lg scroll-smooth">
+      <div class="flex flex-col">
+        <div class="flex-1 overflow-y-auto py-4 ">
+          <div v-if="pool.selectedServer" v-for="channel in pool.activeServerChannels.value" :key="channel.Id">
+            <div class="px-4 py-2 hover:bg-gray-700/50 cursor-pointer flex flex-col"
+              v-on:click="channelSelect(channel.Id)">
+              <ContextMenu>
+                <ContextMenuTrigger>
+                  <div class="flex items-center justify-between group">
+                    <div class="flex items-center space-x-2">
+                      <HashIcon v-if="channel.ChannelType === 'Text'" class="w-5 h-5 text-gray-400" />
+                      <Volume2Icon v-else-if="channel.ChannelType === 'Voice'" class="w-5 h-5 text-gray-400" />
+                      <AntennaIcon v-else-if="channel.ChannelType === 'Announcement'" class="w-5 h-5 text-gray-400" />
+                      <span>{{ channel?.Name }}</span>
+                    </div>
+                  </div>
+                </ContextMenuTrigger>
+                <ContextMenuContent class="w-64">
+                  <ContextMenuItem inset :disabled="!isAdmin" @click="channelDelete(channel.Id)">
+                    Delete
+                    <ContextMenuShortcut>⌘[</ContextMenuShortcut>
                   </ContextMenuItem>
-                  <ContextMenuItem>Create Shortcut...</ContextMenuItem>
-                  <ContextMenuItem>Name Window...</ContextMenuItem>
-                  <ContextMenuSeparator />
-                  <ContextMenuItem>Developer Tools</ContextMenuItem>
-                </ContextMenuSubContent>
-              </ContextMenuSub>
-              <ContextMenuSeparator />
-              <ContextMenuCheckboxItem checked>
-                Show Bookmarks Bar
-                <ContextMenuShortcut>⌘⇧B</ContextMenuShortcut>
-              </ContextMenuCheckboxItem>
-              <ContextMenuCheckboxItem>Show Full URLs</ContextMenuCheckboxItem>
-              <ContextMenuSeparator />
-              <ContextMenuRadioGroup model-value="pedro">
-                <ContextMenuLabel inset>
-                  People
-                </ContextMenuLabel>
-                <ContextMenuSeparator />
-                <ContextMenuRadioItem value="pedro">
-                  Pedro Duarte
-                </ContextMenuRadioItem>
-                <ContextMenuRadioItem value="colm">
-                  Colm Tuite
-                </ContextMenuRadioItem>
-              </ContextMenuRadioGroup>
-            </ContextMenuContent>
-          </ContextMenu>
+                  <ContextMenuItem inset :disabled="!isAdmin">
+                    Leave
+                    <ContextMenuShortcut>⌘]</ContextMenuShortcut>
+                  </ContextMenuItem>
 
+                  <ContextMenuSeparator />
+                  <ContextMenuCheckboxItem :disabled="!isAdmin">
+                   Mute
+                    <ContextMenuShortcut>⌘⇧B</ContextMenuShortcut>
+                  </ContextMenuCheckboxItem>
+                </ContextMenuContent>
+              </ContextMenu>
+            </div>
+            <ul
+              v-if="channel.ChannelType === 'Voice' && pool.realtimeChannelUsers.has(channel.Id) && pool.realtimeChannelUsers.get(channel.Id)?.Users.size != 0"
+              class="ml-3 space-y-2 px-4 pb-2 cursor-pointer flex flex-col">
+              <li v-for="user in pool.realtimeChannelUsers.get(channel.Id)!.Users.values()" :key="user.UserId"
+                class="flex items-center mt-1 text-gray-400 hover:text-white">
+                <ArgonAvatar :fallback="user.User.DisplayName" :fileId="user.User.AvatarFileId!" :userId="user.UserId"
+                  :style="(user.isSpeaking ? 'outline: solid #45d110 2px; outline-offset: 4px; border-radius: 500px;' : '')"
+                  class="w-7 h-7 rounded-full mr-3" />
+                <span>{{ user.User.DisplayName }}</span>
+              </li>
+            </ul>
+
+
+          </div>
         </div>
       </div>
     </div>
+
+    <Dialog v-model:open="addChannelOpened">
+      <DialogContent class="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Add channel</DialogTitle>
+        </DialogHeader>
+        <div class="grid gap-4 py-4">
+          <div class="grid grid-cols-4 items-center gap-4">
+            <Label for="channel-name" class="text-right">
+              Name
+            </Label>
+            <Input id="channel-name" v-model="channelName" class="col-span-3" />
+          </div>
+          <div class="grid grid-cols-4 items-center gap-4">
+            <Label for="channel-type" class="text-right">
+              Type
+            </Label>
+            <RadioGroup id="channel-type" v-model="channelType" :orientation="'vertical'">
+              <div class="flex items-center space-x-2">
+                <RadioGroupItem id="r1" value="Text" />
+                <Label for="r1">
+                  <Badge>Text</Badge>
+                </Label>
+              </div>
+              <div class="flex items-center space-x-2">
+                <RadioGroupItem id="r2" value="Voice" />
+                <Label for="r2">
+                  <Badge>Voice</Badge>
+                </Label>
+              </div>
+              <div class="flex items-center space-x-2">
+                <RadioGroupItem id="r3" value="Announcement" />
+                <Label for="r3">
+                  <Badge>Announcement</Badge>
+                </Label>
+              </div>
+            </RadioGroup>
+          </div>
+        </div>
+        <DialogFooter>
+          <DialogClose as-child>
+            <Button type="button" variant="secondary">
+              Cancel
+            </Button>
+          </DialogClose>
+          <Button type="submit" @click="addChannel">
+            Create Channel
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   </div>
+
 </template>
 
 <script setup lang="ts">
 import { useServerStore } from '@/store/serverStore';
 import {
-  HashIcon, Volume2Icon, PlusIcon, MoreVerticalIcon, Edit2Icon, Trash2Icon,
-  CopyCheckIcon, MessageSquareLock, CloverIcon, AntennaIcon
+  HashIcon, Volume2Icon, MoreVerticalIcon, AntennaIcon
 } from 'lucide-vue-next';
 import {
   ContextMenu,
   ContextMenuCheckboxItem,
   ContextMenuContent,
   ContextMenuItem,
-  ContextMenuLabel,
-  ContextMenuRadioGroup,
-  ContextMenuRadioItem,
   ContextMenuSeparator,
   ContextMenuShortcut,
-  ContextMenuSub,
-  ContextMenuSubContent,
-  ContextMenuSubTrigger,
   ContextMenuTrigger,
-} from '@/components/ui/context-menu'
+} from '@/components/ui/context-menu';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 import { Badge } from '@/components/ui/badge'
 import {
   Dialog,
@@ -182,20 +158,21 @@ import {
   DialogFooter,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
   DialogClose,
 } from '@/components/ui/dialog'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Button } from '@/components/ui/button'
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import { logger } from '@/lib/logger';
 import { useWindow } from '@/store/windowStore';
 import { usePoolStore } from '@/store/poolStore';
 import { useVoice } from '@/store/voiceStore';
 import ArgonAvatar from './ArgonAvatar.vue';
+import { useMe } from '@/store/meStore';
 
+const serverDropdownIsActive = ref(false);
 const channelType = ref("" as "Text" | "Voice" | "Announcement");
 const channelName = ref("");
 const addChannelOpened = ref(false);
@@ -204,6 +181,10 @@ const servers = useServerStore();
 const windows = useWindow();
 const pool = usePoolStore();
 const voice = useVoice();
+const me = useMe();
+
+
+const isAdmin = computed(() => me.me?.Username === "yuuki");
 
 
 const addChannel = () => {
@@ -234,6 +215,11 @@ async function channelSelect(channelId: string) {
   }*/
 }
 
+async function openServerSettings() {
+  serverDropdownIsActive.value = false;
+  windows.serverSettingsOpen = true;
+}
+
 async function channelDelete(channelId: string) {
   await servers.deleteChannel(channelId);
 }
@@ -245,10 +231,38 @@ const connectToChannel = (channelId: string) => {
 </script>
 
 <style scoped>
+.controls-list {
+  background-color: #161616;
+  align-items: center;
+  justify-content: space-between;
+  position: relative;
+}
+
+.control-bar {
+  justify-content: center;
+  display: flex;
+  gap: 10px;
+  flex: auto;
+}
+
+.controls button {
+  background: none;
+  border: none;
+  color: #fff;
+  font-size: 16px;
+  cursor: pointer;
+  margin-left: 5px;
+  transition: color 0.3s;
+  margin: 5px;
+}
+
+.header-list {
+  background-color: #161616;
+  padding-top: 10px;
+}
+
 .chat-list {
   background-color: #161616;
-  padding: 10px;
-  border-radius: 15px;
   box-shadow: 0 4px 12px rgba(0, 0, 0, 0.15);
   margin-bottom: 10px;
   height: 95%;
