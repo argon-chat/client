284f0f8707f8e5436ce4b1116356f8b40a57b733
Yuuki Wesp <ls-micro@ya.ru>
2025-03-04 20:07:16 +0300

    Add streamSettingOpen to window store
    - Added a new ref for stream settings in the window store.

--- src/store/windowStore.ts
+++ src/store/windowStore.ts
@@ -4,6 +4,7 @@ import { ref } from "vue";
 export const useWindow = defineStore("window", () => {
   const settingsOpen = ref(false);
   const serverSettingsOpen = ref(false);
+  const streamSettingOpen = ref(false);
 
   return { settingsOpen, serverSettingsOpen };
 });
