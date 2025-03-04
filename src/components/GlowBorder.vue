ede759bd6399261fc3be702d33a0abfb2292c0d4
Yuuki Wesp <ls-micro@ya.ru>
2025-03-04 20:07:54 +0300

    Refactor CSS class in GlowBorder component - Updated CSS class to remove unnecessary padding in the component.

--- src/components/GlowBorder.vue
+++ src/components/GlowBorder.vue
@@ -1,6 +1,6 @@
 <template>
     <div :style="parentStyles" :class="cn(
-        'relative grid min-h-[60px] w-fit min-w-[300px] place-items-center rounded-[--border-radius] bg-white p-3 text-black dark:bg-black dark:text-white glow-border',
+        'relative grid min-h-[60px] w-fit min-w-[300px] place-items-center rounded-[--border-radius] bg-white text-black dark:bg-black dark:text-white glow-border',
         $props.class,
     )
         ">
