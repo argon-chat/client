0cdc3b14ef0e93f5853e391996ad1d2a572f0db3
Yuuki Wesp <ls-micro@ya.ru>
2025-03-09 10:50:00 +0300

    Update hotKeyStore with new HotKeyAction properties and availableActions, refactor useHotkeys to handle hotkey verification and action subscription.

--- src/store/hotKeyStore.ts
+++ src/store/hotKeyStore.ts
@@ -1,7 +1,7 @@
 import { logger } from "@/lib/logger";
 import { persistedValue } from "@/lib/persistedValue";
 import { defineStore } from "pinia";
-import { Subject } from "rxjs";
+import { filter, Subject, Subscription } from "rxjs";
 import { ref } from "vue";
 
 export type HotKeyMod = {
@@ -11,27 +11,76 @@ export type HotKeyMod = {
   hasAlt: boolean;
 };
 
+export type HotketActionGroup = {
+  actionKey: string;
+  group: string;
+  disabled: boolean;
+};
+
 export type HotKeyAction = {
-    actorKey: string;
-    fullHotKeyString: string;
-    mod: HotKeyMod;
-    keyCode: number;
-    localeKeyCode: string;
-}
+  actionKey: string;
+  mod: HotKeyMod | null;
+  keyCode: number;
+  isGlobal: boolean;
+  errText?: string;
+  group: string;
+  disabled: boolean;
+};
+
+export const availableActions = [
+  { actionKey: "key.microphone.toggle" as const, group: "audio", disabled: false },
+  { actionKey: "key.microphone.on" as const, group: "audio", disabled: false },
+  { actionKey: "key.microphone.off" as const, group: "audio", disabled: false },
+  { actionKey: "key.sound.toggle" as const, group: "audio", disabled: true },
+  { actionKey: "key.sound.on" as const, group: "audio", disabled: true },
+  { actionKey: "key.sound.off" as const, group: "audio", disabled: true },
+] satisfies HotketActionGroup[];
+
+export type ActionKey = (typeof availableActions)[number]["actionKey"];
 
 export const useHotkeys = defineStore("hotkeys", () => {
-  const dict = new Map<number, string>();
-  const hotkeyExecuted = new Subject<string>();
+  const dictHandlers = new Map<number, string>();
+  const dictActions = new Map<string, number>();
+  const hotkeyExecuted = new Subject<ActionKey>();
   const isPaused = ref(false);
 
-  const allHotKeys = persistedValue<HotKeyAction[]>("HotKeyAction_all", []);
+  const allHotKeys = persistedValue<Map<string, HotKeyAction>>(
+    "HotKeyAction_all",
+    new Map()
+  );
 
-  function onHotKeyCalled(key: number) {
-    if (isPaused) return;
-    if (dict.has(key)) hotkeyExecuted.next(dict.get(key)!);
+  for (let i of availableActions) {
+    if (!allHotKeys.has(i.actionKey)) {
+      allHotKeys.set(i.actionKey, {
+        actionKey: i.actionKey,
+        isGlobal: true,
+        keyCode: 0,
+        mod: null,
+        group: i.group,
+        disabled: i.disabled
+      });
+    } else {
+      const hte = allHotKeys.get(i.actionKey);
+
+      if (hte) hte.errText = undefined;
+    }
   }
 
-  const pinnedHotKeyCallback = native.createPinnedObject(onHotKeyCalled);
+  function onHotKeyCalled(key: number) {
+    logger.log(`onHotKeyCalled: ${key}`, dictHandlers, dictActions, isPaused);
+    //if (isPaused) return;
+    logger.log(`onHotKeyCalled (no paused): ${key}`);
+    if (dictHandlers.has(key)) {
+      const eta = dictHandlers.get(key)! as any;
+      logger.log(`onHotKeyCalled (no paused): ${key}, eta: ${eta}`);
+
+      hotkeyExecuted.next(dictHandlers.get(key)! as any);
+    }
+  }
+
+  const pinnedHotKeyCallback = argon.isArgonHost
+    ? native.createPinnedObject(onHotKeyCalled)
+    : ({} as any);
 
   const enum HotkeyModification {
     NONE = 0,
@@ -50,28 +99,70 @@ export const useHotkeys = defineStore("hotkeys", () => {
     );
   }
 
-  async function createHotKey(keycode: number, action: string, mod: HotKeyMod) {
-    try {
-      const i = await native.createKeybind(
-        {
-          keyCode: keycode,
-          keyMod: encodeHotkeyModification(mod),
-        },
-        pinnedHotKeyCallback
+  async function doVerifyHotkeys() {
+    if (argon.isArgonHost) native.clearAllKeybinds();
+    dictActions.clear();
+
+    for (let i of allHotKeys.values()) {
+      if (i.keyCode == 0) {
+        if (i.errText) i.errText = undefined;
+        continue;
+      }
+
+      if (dictActions.has(i.actionKey)) {
+        if (i.errText) i.errText = undefined;
+        continue; // TODO verify combination
+      }
+
+      logger.log(
+        `DoVerify, call bind ${i.actionKey} ->> ${i.keyCode}+${i.mod}`
       );
 
-      dict.set(i, action);
-      return true;
-    } catch (e) {
-      logger.error(e);
-      return false;
+      try {
+        const result = await createHotKey(i.keyCode, i.actionKey, i.mod);
+
+        if (!result) {
+          allHotKeys.get(i.actionKey)!.errText = "unknown error";
+          continue;
+        }
+      } catch (e) {
+        allHotKeys.get(i.actionKey)!.errText = `${e}`;
+      }
     }
   }
+
+  async function createHotKey(
+    keycode: number,
+    action: string,
+    mod: HotKeyMod | null
+  ) {
+    const i = await native.createKeybind(
+      {
+        keyCode: keycode,
+        keyMod: mod ? encodeHotkeyModification(mod) : 0,
+      },
+      pinnedHotKeyCallback
+    );
+
+    dictHandlers.set(i, action);
+    dictActions.set(action, i);
+    return true;
+  }
+
+  function onAction(key: ActionKey, func: () => void): Subscription {
+    return hotkeyExecuted
+      .pipe(filter((event) => event === key))
+      .subscribe(func);
+  }
+
+  doVerifyHotkeys();
 
   return {
     createHotKey,
     hotkeyExecuted,
     isPaused,
-    allHotKeys
+    allHotKeys,
+    doVerifyHotkeys,
+    onAction,
   };
 });
