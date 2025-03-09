863187738303b40a80a96cf949a2500702b7b123
Yuuki Wesp <ls-micro@ya.ru>
2025-03-09 10:50:04 +0300

    Refactor locale handling and add persisted value feature
    - Added persistedValue import for storing locale
    - Updated useLocale store with currentLocale and updateLocale functions
    - Implemented watch on currentLocale to update locale value

--- src/store/localeStore.ts
+++ src/store/localeStore.ts
@@ -1,10 +1,34 @@
-import { MessageSchema } from "@/locales";
+import { persistedValue } from '@/lib/persistedValue';
+import { locales, Locale, LocaleSchema } from '@/locales'
 import { defineStore } from "pinia";
+import { watch } from 'vue';
 import { useI18n } from "vue-i18n";
 
 export const useLocale = defineStore("locale", () => {
-  const { t } = useI18n<{ message: MessageSchema }>();
+
+  const currentLocale = persistedValue<string>("locale", "en");
+
+  const { t, locale } = useI18n<[LocaleSchema], Locale>({
+    locale: 'en',
+    fallbackLocale: 'en',
+    messages: locales
+  });
+
+
+  function updateLocale(key: string) {
+    currentLocale.value = key as any;
+  }
+
+
+  locale.value = currentLocale.value as any;
+
+  watch(currentLocale, (x) => {
+    locale.value = x as any;
+  });
+
   return {
     t,
+    currentLocale,
+    updateLocale
   };
 });
