5035a6f4ba2bd4e3c45b9042fd296a82be21daa8
Yuuki Wesp <ls-micro@ya.ru>
2025-04-19 23:22:49 +0300

    fix argon-chat/argon#76

--- src/views/CreateOrJoin.vue
+++ src/views/CreateOrJoin.vue
@@ -65,15 +65,11 @@ const createServer = () => {
 
 <style scoped>
 .modal-overlay {
-    position: fixed;
-    top: 0;
-    left: 0;
     width: 100vw;
     height: 100vh;
     display: flex;
     align-items: center;
     justify-content: center;
-    z-index: 1000;
 }
 
 .modal-content {
