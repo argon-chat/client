2227eb5a0b25082f93bcc4ab78939ebcdbe8763f
Yuuki Wesp <ls-micro@ya.ru>
2025-04-27 07:03:15 +0300

    Add interval for heartbeat and user status in event handling. Use ref for intervalSubject.

--- src/store/busStore.ts
+++ src/store/busStore.ts
@@ -1,14 +1,21 @@
 import { defineStore } from "pinia";
-import { Subject, Subscription } from "rxjs";
-import { filter } from "rxjs/operators";
+import { interval, Subject, Subscription } from "rxjs";
+import { filter, switchMap } from "rxjs/operators";
 import { useApi } from "./apiStore";
 import { logger } from "@/lib/logger";
+import { ref } from "vue";
+import { useLocalStorage } from "@vueuse/core";
+import { UserStatus } from "@/lib/glue/UserStatus";
 
 export type EventWithServerId<T> = {serverId: string} & T;
 
 export const useBus = defineStore("bus", () => {
   const argonEventBus = new Subject<{serverId: string} & IArgonEvent>();
   const userEventBus = new Subject<IArgonEvent>();
+  const intervalSubject = ref(null as Subscription | null);
+  const preferredStatus = useLocalStorage<UserStatus>("preferredStatus", "Online", { initOnMounted: true, listenToStorageChanges: true, writeDefaults: true });
+
+  
 
   const controller = new AbortController();
   const { signal } = controller;
@@ -16,9 +23,16 @@ export const useBus = defineStore("bus", () => {
   const api = useApi();
 
   async function doListenServer(id: string) {
+    if (!intervalSubject.value) {
+      logger.warn("CREATED SUBJECT INTERVAL")
+      intervalSubject.value = interval(2000).pipe(switchMap(() => {
+        return sendHeartbeat();
+      })).subscribe();
+    }
+
     const handle = await api.eventBus.SubscribeToServerEvents(id);
     for await(const e of handle) {
-      if (e.EventKey !== "UserChangedStatus")
+      //if (e.EventKey !== "UserChangedStatus")
         logger.log(`Received event, ${e.EventKey}`, e);
       argonEventBus.next({ serverId: id, ...e });
       if (signal.aborted)
@@ -29,12 +43,17 @@ export const useBus = defineStore("bus", () => {
   async function doListenMyEvents() {
     const handle = await api.eventBus.SubscribeToMeEvents();
     for await(const e of handle) {
-      if (e.EventKey !== "UserChangedStatus")
+      //if (e.EventKey !== "UserChangedStatus")
         logger.log(`Received event, ${e.EventKey}`, e);
       userEventBus.next(e);
       if (signal.aborted)
         break;
     }
+  }
+
+  async function sendHeartbeat()
+  {
+    await api.getRawClient().value.sendPackageToActieTransport({ EventKey: "HeartBeatEvent", status: preferredStatus.value } as HeartBeatEvent)
   }
 
   function listenEvents(id: string) {
