9a292f69ce8bb9543f8f315a675b5c736a0e7c11
Yuuki Wesp <ls-micro@ya.ru>
2025-04-27 07:03:25 +0300

    Refactor user status handling and add status change function
    - Refactored user status handling to use ExtendedUser type
    - Added function to change user status based on UserStatus enum

--- src/store/meStore.ts
+++ src/store/meStore.ts
@@ -4,13 +4,30 @@ import { ref } from "vue";
 import { useBus } from "./busStore";
 import { logger } from "@/lib/logger";
 import { setUser } from "@sentry/vue";
+import { UserStatus } from "@/lib/glue/UserStatus";
+import { useLocalStorage } from "@vueuse/core";
+import { useIdle } from '@vueuse/core'
 
-export type ExtendedUser = { currentStatus: 'Offline' | 'Online' | 'Away' | 'InGame' | 'Listen' | 'TouchGrass' | 'DoNotDisturb' } & IUser;
+export type ExtendedUser = {
+  currentStatus:
+    | "Offline"
+    | "Online"
+    | "Away"
+    | "InGame"
+    | "Listen"
+    | "TouchGrass"
+    | "DoNotDisturb";
+} & IUser;
 
 export const useMe = defineStore("me", () => {
   const api = useApi();
   const bus = useBus();
-  const me = ref(null as (ExtendedUser | null));
+  const me = ref(null as ExtendedUser | null);
+  const preferredStatus = useLocalStorage<UserStatus>(
+    "preferredStatus",
+    "Online",
+    { initOnMounted: true, listenToStorageChanges: true, writeDefaults: true }
+  );
 
   const WelcomeCommanderHasReceived = ref(false);
 
@@ -18,14 +35,18 @@ export const useMe = defineStore("me", () => {
     return await api.userInteraction.GetMe();
   }
 
+  async function changeStatusTo(status: UserStatus) {
+    preferredStatus.value = status;
+    if (me.value) me.value!.currentStatus = status;
+  }
 
   async function init() {
-    me.value = { currentStatus: "Online", ...(await getMe()) };
+    me.value = { currentStatus: preferredStatus.value, ...(await getMe()) };
 
     logger.info("Received user info ", me.value);
     bus.onUserEvent<WelcomeCommander>("WelcomeCommander", (e) => {
-        logger.box(`Welcome commander, ${e.welcomeMessage}`);
-        me.value!.currentStatus = e.status;
+      logger.box(`Welcome commander, ${e.welcomeMessage}`);
+      me.value!.currentStatus = e.status;
     });
     WelcomeCommanderHasReceived.value = true;
     bus.doListenMyEvents();
@@ -33,9 +54,28 @@ export const useMe = defineStore("me", () => {
     setUser({ id: me.value.Id, username: me.value.Username });
   }
 
+  const statusClass = (status: UserStatus | string, useBg = true) => {
+    if (useBg)
+      return {
+        "bg-green-500": status === "Online",
+        "bg-yellow-500": status === "Away",
+        "bg-gray-500": status === "Offline",
+        "bg-red-500": status === "DoNotDisturb",
+      };
+    else
+      return {
+        "text-green-500": status === "Online",
+        "text-yellow-500": status === "Away",
+        "text-gray-500": status === "Offline",
+        "text-red-500": status === "DoNotDisturb",
+      };
+  };
+
   return {
     me,
     init,
-    WelcomeCommanderHasReceived
+    WelcomeCommanderHasReceived,
+    changeStatusTo,
+    statusClass,
   };
 });
