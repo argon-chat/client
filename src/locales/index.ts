9607601bed8b2e8b340ad3f6cc8518ccc1dc10ff
Yuuki Wesp <ls-micro@ya.ru>
2025-04-28 12:44:01 +0300

    Add Armenian language support with new translations. Update the index to include the new language file.

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,13 +1,15 @@
 import en from './en.json'
 import ru from './ru.json'
 import jp from './jp.json'
+import am from './am.json'
 import ruPt from './ru_pt.json'
 
 export const locales = {
   en,
   ru,
   ru_pt: ruPt,
-  jp
+  jp,
+  am
 } as const
 
 export type Locale = keyof typeof locales
