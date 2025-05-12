8fe56161c016ecbc3c90b1351f7415368ca58391
Yuuki Wesp <ls-micro@ya.ru>
2025-05-12 21:13:34 +0300

    Refactor underline color handling in UnderlineSegment.vue
    Update the logic for setting text-decoration-color based on entity.Color value. Simplified code for better readability.

--- src/components/chats/UnderlineSegment.vue
+++ src/components/chats/UnderlineSegment.vue
@@ -1,5 +1,7 @@
 <template>
-    <span :class="['underline', colour]">{{ props.text }}</span>
+    <span :class="['underline', colour]" :style="{
+        'text-decoration-color': props.entity.Colour == 0 ? '' : `#${(props.entity.Colour & 0xFFFFFF).toString(16).padStart(6, '0')}`
+    }">{{ props.text }}</span>
 </template>
 <script setup lang="ts" generic="T extends IMessageEntityUnderline">
 import { computed } from 'vue';
@@ -13,6 +15,6 @@ const props = defineProps<{
 const colour = computed(() => 
     props.entity.Colour == 0 ? 
     'decoration-blue-600' : 
-    `decoration-[#${(props.entity.Colour & 0xFFFFFF).toString(16).padStart(6, '0')}]`)
+    ``);
 
 </script>
