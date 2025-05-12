3313a324a5444d36594a94735a4d72014e3a68c7
Yuuki Wesp <ls-micro@ya.ru>
2025-05-12 21:13:40 +0300

    Add HotKey component for displaying keyboard shortcuts.
    - Created a new Vue component to display keyboard shortcuts using provided props.
    - Styled the component to show keys with separators between them.

---
+++ src/components/HotKey.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="inline-flex items-center">
+        <kbd v-for="(i, x) in keys" :key="x" class="hotkey-kbd">
+            {{ i }}
+        </kbd>
+    </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    keys: string[]
+}>();
+</script>
+<style scoped>
+.hotkey-kbd {
+    @apply px-1.5 py-0.5 rounded border text-xs shadow;
+    position: relative;
+    margin-right: 0.55rem;
+}
+
+.hotkey-kbd:not(:last-child)::after {
+    content: "+";
+    position: absolute;
+    right: -0.65rem;
+    top: 50%;
+    transform: translateY(-50%);
+    font-size: 0.85rem;
+    color: theme("colors.muted.foreground");
+    margin-left: 0.25rem;
+}
+</style>
