7f713de80c76621a134357df21b44dd129592737
Yuuki Wesp <ls-micro@ya.ru>
2025-05-29 03:31:54 +0300

    Add WebRTC audio processing capabilities
    - Added a new class for WebRTC audio processing with methods to initialize, restart, and destroy the audio processing pipeline. The class handles converting stereo tracks to mono and managing audio worklets for real-time processing.

---
+++ src/lib/audio/WebRTCProcessor.ts
@@ -0,0 +1,82 @@
+import { usePreference } from "@/store/preferenceStore";
+import { AudioProcessorOptions, Room, Track, TrackProcessor } from "livekit-client";
+import { DisposableBag } from "../disposables";
+import { IAudioManagement } from "./AudioManager";
+import { worklets } from "./WorkletBase";
+
+export class WebRTCProcessor
+  implements TrackProcessor<Track.Kind.Audio, AudioProcessorOptions>
+{
+  private bag: DisposableBag = new DisposableBag();
+  private worklets: AudioWorkletNode[];
+  private audio: IAudioManagement;
+  constructor(audio: IAudioManagement) {
+    this.worklets = [];
+    this.audio = audio;
+  }
+
+  name = "audio-processor";
+  async init(opts: AudioProcessorOptions): Promise<void> {
+    let { track } = opts;
+
+    const prefs = usePreference();
+
+    const sub = prefs.onforceToMonoChanged.subscribe((x) => {
+      this.worklets.forEach((element) => {
+        if (element.parameters.has("enabled")) {
+          worklets.setEnabledVUNode(element, x);
+        }
+      });
+    });
+
+    this.bag.addSubscription(sub);
+
+    const w1 = (await worklets.createStereoToMonoProcessor()).injectInto(
+      this.bag
+    );
+
+    worklets.setEnabledVUNode(w1, prefs.forceToMono);
+
+    this.worklets = [w1];
+
+    const inputStream = new MediaStream([track]);
+    const sourceNode = new MediaStreamAudioSourceNode(
+      this.audio.getCurrentAudioContext(),
+      {
+        mediaStream: inputStream,
+      }
+    );
+    sourceNode.connect(this.worklets[0]);
+
+    for (let i = 0; i < this.worklets.length - 1; i++) {
+      this.worklets[i].connect(this.worklets[i + 1]);
+    }
+
+    const destinationNode = new MediaStreamAudioDestinationNode(
+      this.audio.getCurrentAudioContext(),
+      {
+        channelCount: 2,
+      }
+    );
+
+    this.worklets[this.worklets.length - 1].connect(destinationNode);
+    const trackOut = destinationNode.stream.getAudioTracks()[0];
+
+    this.processedTrack = trackOut;
+  }
+  async restart(opts: AudioProcessorOptions): Promise<void> {
+    await this.destroy();
+    await this.init(opts);
+  }
+  async destroy(): Promise<void> {
+    await this.bag.asyncDispose();
+    if (this.processedTrack) {
+      this.processedTrack.stop();
+      this.processedTrack = undefined;
+    }
+  }
+
+  processedTrack?: MediaStreamTrack | undefined;
+  onPublish?: ((room: Room) => Promise<void>) | undefined;
+  onUnpublish?: (() => Promise<void>) | undefined;
+}
