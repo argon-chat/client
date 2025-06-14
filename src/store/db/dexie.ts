b2533cf91279a0fe3a143975fce09ce25f307bb7
Yuuki Wesp <ls-micro@ya.ru>
2025-06-14 04:40:16 +0300

    Add archetypes and members tables, update database version to v3.
    Expanded RealtimeUser type to include archetypes. Added archetypes and members tables to the database schema. Updated database version from v2 to v3 for PoolDatabase class.

--- src/store/db/dexie.ts
+++ src/store/db/dexie.ts
@@ -1,33 +1,30 @@
-import { UserStatus } from '@/lib/glue/UserStatus';
 import Dexie, { Table } from 'dexie';
 
-export type RealtimeUser = IUserDto & { status: UserStatus, activity?: IUserActivityPresence };
-
+export type RealtimeUser = IUserDto & { status: UserStatus, activity?: IUserActivityPresence, archetypes?: IArchetypeDto[] };
 
 const tryDropOldDb = (s: string) => { try { indexedDB.deleteDatabase(s) } catch {} };
-
 
 export class PoolDatabase extends Dexie {
   users!: Table<RealtimeUser, Guid>;
   servers!: Table<IServerDto, Guid>;
   channels!: Table<IChannel, Guid>;
   messages!: Table<IArgonMessageDto, number>;
+  archetypes!: Table<IArchetypeDto, Guid>;
+  members!: Table<IServerMemberDto, Guid>;
 
   constructor() {
-    super('argon-db-v2');
+    super('argon-db-v3');
     this.version(1).stores({
       users: 'UserId',
       servers: 'Id',
       channels: 'Id, ServerId',
-    });
-    this.version(2).stores({
-      users: 'UserId',
-      servers: 'Id',
-      channels: 'Id, ServerId',
       messages: 'MessageId, [ChannelId+MessageId], [ServerId+ChannelId+MessageId]',
+      archetypes: 'Id, ServerId, [Id+ServerId]',
+      members: 'MemberId, [MemberId+UserId], [UserId+ServerId], [MemberId+UserId+ServerId], [MemberId+ServerId]'
     });
 
     tryDropOldDb("argon-db");
+    tryDropOldDb("argon-db-v2");
   }
 }
 
