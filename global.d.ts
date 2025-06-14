da83f9dc249efac8b1aa2327ecb8151f0b78a250
Yuuki Wesp <ls-micro@ya.ru>
2025-06-14 05:57:02 +0300

    drop unused files

--- global.d.ts
+++ global.d.ts
@@ -46,3 +46,79 @@ declare module '@vue/runtime-core' {
         vPex: Directive<HTMLElement, ArgonEntitlementUnion>;
     }
 }
+declare global {
+  interface NavigatorStorageBuckets {
+    readonly storageBuckets: StorageBucketManager;
+  }
+
+  interface WorkerNavigatorStorageBuckets {
+    readonly storageBuckets: StorageBucketManager;
+  }
+
+  interface Navigator extends NavigatorStorageBuckets {}
+  interface WorkerNavigator extends WorkerNavigatorStorageBuckets {}
+
+  interface StorageBucketManager {
+    open(name: string, options?: StorageBucketOptions): Promise<StorageBucket>;
+    keys(): Promise<string[]>;
+    delete(name: string): Promise<void>;
+  }
+
+  interface StorageBucketOptions {
+    persisted?: boolean;
+    quota?: number;
+    expires?: DOMHighResTimeStamp;
+  }
+
+  interface ExtendedStorageManager extends StorageBucket {
+    getFileHandle(name: string, opt?: any): Promise<FileSystemHandle>; 
+  }
+
+  interface StorageBucket extends StorageManager {
+    readonly name: string;
+
+    setExpires(expires: DOMHighResTimeStamp): Promise<void>;
+    expires(): Promise<DOMHighResTimeStamp | null>;
+
+    readonly indexedDB: IDBFactory;
+    readonly caches: CacheStorage;
+
+    getDirectory(): Promise<FileSystemDirectoryHandle>;
+  }
+}
+
+declare global {
+  interface WebSocketConnection<
+    T extends Uint8Array | string = Uint8Array | string,
+  > {
+    readable: ReadableStream<T>;
+    writable: WritableStream<T>;
+    protocol: string;
+    extensions: string;
+  }
+  interface WebSocketCloseInfo {
+    closeCode?: number;
+    reason?: string;
+  }
+  interface WebSocketStreamOptions {
+    protocols?: string[];
+    signal?: AbortSignal;
+  }
+
+  declare class WebSocketStream<
+    T extends Uint8Array | string = Uint8Array | string,
+  > {
+    readonly url: string;
+    readonly opened: Promise<WebSocketConnection<T>>;
+    readonly closed: Promise<WebSocketCloseInfo>;
+    readonly close: (closeInfo?: WebSocketCloseInfo) => void;
+    constructor(url: string, options?: WebSocketStreamOptions);
+  }
+
+  interface Window {
+    ui_version: string;
+    ui_buildtime: string;
+    ui_fullversion: string;
+    ui_branch: string;
+  }
+}
