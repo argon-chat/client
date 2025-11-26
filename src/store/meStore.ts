bee27b53b7ea1cdb5408dfa85c52a95ab0c42885
Yuuki Wesp <ls-micro@ya.ru>
2025-11-26 21:10:49 +0300

    add log for GetMyAuthorization

--- src/store/meStore.ts
+++ src/store/meStore.ts
@@ -63,6 +63,8 @@ export const useMe = defineStore("me", () => {
       authStore.getRefreshToken()
     );
 
+    logger.error("GetMyAuthorization", result);
+
     if (result.isBadAuthStatus()) {
       useAuthStore().logout();
       location.reload();
