dfe53362c0de29c7f2aceea200507855b7872cdf
Yuuki Wesp <ls-micro@ya.ru>
2026-01-19 21:44:37 +0300

    dialog fixes

--- packages/ui/src/dialog/DialogScrollContent.vue
+++ packages/ui/src/dialog/DialogScrollContent.vue
@@ -6,11 +6,11 @@ import { X } from "lucide-vue-next"
 import {
   DialogClose,
   DialogContent,
-  DialogOverlay,
   DialogPortal,
   useForwardPropsEmits,
 } from "reka-ui"
 import { cn } from "@argon/core"
+import DialogOverlay from "./DialogOverlay.vue"
 
 defineOptions({
   inheritAttrs: false,
@@ -26,13 +26,11 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits)
 
 <template>
   <DialogPortal>
-    <DialogOverlay
-      class="fixed inset-0 z-50 grid place-items-center overflow-y-auto bg-black/80  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0"
-    >
+    <DialogOverlay class="grid place-items-center overflow-y-auto py-8">
       <DialogContent
         :class="
           cn(
-            'relative z-50 grid w-full max-w-lg my-8 gap-4 border border-border bg-background p-6 shadow-lg duration-200 sm:rounded-lg md:w-full',
+            'relative z-50 grid w-full max-w-lg gap-4 border border-border bg-background p-6 shadow-lg duration-200 sm:rounded-lg md:w-full data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:pointer-events-none',
             props.class,
           )
         "
